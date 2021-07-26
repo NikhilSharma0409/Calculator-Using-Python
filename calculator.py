ea765c1e023837b0bd128187407466256ae9f6e4
NIKHIL SHARMA <87971380+NikhilSharma0409@users.noreply.github.com>
2021-07-26 22:10:20 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,38 @@
+
+num1 = int(input("Enter First Number "))
+
+operation = input("Enter Operation ")
+
+num2 = int(input("Enter Second Number "))
+
+while True:
+    if operation == '+':
+        print(num1 + num2)
+
+    elif operation == '-':
+        print(num1 - num2)
+
+    elif operation == '*':
+        print(num1 * num2)
+
+    elif operation == '/':
+        print(num1 / num2)
+
+    elif operation == '//':
+        print(num1 // num2)
+
+    elif operation == '**':
+        print(num1 ** num2)
+
+    else:
+        print("Syntax Error")
+
+
+num1 = int(input("Enter First Number "))
+
+operation = input("Enter Operation ")
+
+num2 = int(input("Enter Second Number "))
+
+
+    
