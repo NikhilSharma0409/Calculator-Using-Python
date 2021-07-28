41d6b0e69a29f3a7779346b966c5682ec3352b55
NIKHIL SHARMA <87971380+NikhilSharma0409@users.noreply.github.com>
2021-07-28 08:44:55 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,11 +1,9 @@
 
-num1 = int(input("Enter First Number "))
-
-operation = input("Enter Operation ")
-
-num2 = int(input("Enter Second Number "))
-
 while True:
+    num1 = int(input("Enter First Number "))
+    operation = input("Enter Operation ")
+    num2 = int(input("Enter Second Number "))
+
     if operation == '+':
         print(num1 + num2)
 
@@ -26,13 +24,6 @@ while True:
 
     else:
         print("Syntax Error")
-
-
-num1 = int(input("Enter First Number "))
-
-operation = input("Enter Operation ")
-
-num2 = int(input("Enter Second Number "))
 
 
     
